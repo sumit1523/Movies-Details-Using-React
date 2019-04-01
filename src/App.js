f3100bc93dedcc19ce484fa267ed24b40f111184
Ramachandrappa, Sowmya <Sowmya.Ramachandrappa@meijer.com>
2019-04-01 14:33:20 +0530

    Creating component in react

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const welcome = "Welcome to React"
+
 class App extends Component {
   render() {
     return (
@@ -11,6 +13,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <Welcome text="Welcome to React"/>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -25,4 +28,11 @@ class App extends Component {
   }
 }
 
+class Welcome extends Component{
+  render(){
+    return(
+      <h1 className="App-title">{welcome}</h1>
+    )
+  }
+}
 export default App;
