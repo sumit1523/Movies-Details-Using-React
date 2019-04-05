0327104637d78f63dcee44839aec7fdb1c0329b1
sumit kumar <sumit.u.kumar@capgemini.com>
2019-04-05 12:15:32 +0530

    React overdrive

--- src/Movies.js
+++ src/Movies.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import Overdrive from 'react-overdrive';
 
 const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
 
 const Movies=({movie})=> (  
 
     <Link to={`/${movie.id}`}>
-        <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title}/>       
+        <Overdrive id={movie.id}>
+            <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title}/>       
+        </Overdrive>
     </Link>
 
 );
