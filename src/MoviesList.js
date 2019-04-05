862ca2ae4d09ff41d6dd58ff3fd0dc5820fc8d63
sumit kumar <sumit.u.kumar@capgemini.com>
2019-04-05 12:22:09 +0530

    PureComponent

--- src/MoviesList.js
+++ src/MoviesList.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import Movies from './Movies';
 import styled from 'styled-components';
-class MoviesList extends Component {
+class MoviesList extends PureComponent {
   
   state = {
     movies: []
