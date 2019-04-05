3175e7e0d36edbb3597ef7b07be37d4f3ba9db1d
sumit kumar <sumit.u.kumar@capgemini.com>
2019-04-05 14:28:35 +0530

    Updates

---
+++ precache-manifest.38eab1accbb330a2c2c1adb3dac9e00c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sumit1523/Movies-Details-Using-React/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1c23bc35fc1253181da8",
+    "url": "/sumit1523/Movies-Details-Using-React/static/js/runtime~main.7008e4d5.js"
+  },
+  {
+    "revision": "e396520560fd719239a9",
+    "url": "/sumit1523/Movies-Details-Using-React/static/js/main.18c0b921.chunk.js"
+  },
+  {
+    "revision": "6f0e5aeed99b79434d1a",
+    "url": "/sumit1523/Movies-Details-Using-React/static/js/2.838a94a8.chunk.js"
+  },
+  {
+    "revision": "e396520560fd719239a9",
+    "url": "/sumit1523/Movies-Details-Using-React/static/css/main.8d54baed.chunk.css"
+  },
+  {
+    "revision": "8ac36a0a446f831b19c51a32efb83545",
+    "url": "/sumit1523/Movies-Details-Using-React/index.html"
+  }
+];
